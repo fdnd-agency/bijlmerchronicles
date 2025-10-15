19466858f4f12056b359129d7b6d9e8e72766f26
Matthijs ten Brink <matthijstenbrink@gmail.com>
2025-10-15 22:24:30 +0200

    lemma cards af #56
    Lemma cards helemaal af, responsive, accessible en een hover voor de titel.

---
+++ src/lib/components/Lemmacard.svelte
@@ -0,0 +1,63 @@
+<script>
+    export let imgname = "lemmapngtest1";
+</script>
+
+<li>
+    <a href="/">
+        <img src="/{imgname}.png" alt="Ga naar de Johan Cruijff Arena op de map">
+        <h2>Johan Cruijff Arena</h2>
+    </a>
+</li>
+
+<style>
+    li {
+        flex: 0 0 10%;
+        scroll-snap-align: center;
+        list-style-type: none;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        &:nth-of-type(1n) {
+            background-color: #BFCFB3;
+        }
+        &:nth-of-type(2n) {
+            background-color: #CEBF8F;
+        }
+    }
+    a {
+        position: relative;
+        display: block;
+        width: 200px;
+        height: 100px;
+        border-radius: 8px;
+        box-shadow: 5px 5px 0px rgb(0, 0, 0);
+        h2 {
+            opacity: 0  ;
+            display: block;
+            color: rgb(255, 255, 255);
+            position: absolute;
+            bottom: 2px;
+            left: 50%;
+            transform: translateX(-50%) translateY(10px) scale(1.05);
+            margin: 0;
+            justify-self: center;
+            white-space: nowrap;
+            font-size: 1.25rem;
+            text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.7);
+            transition:
+                opacity 0.25s ease-out,
+                transform 0.25s ease-out;
+        }
+        &:hover h2 {
+            opacity: 1;
+            transform: translateX(-50%) translateY(0) scale(1.05);
+        }
+    }
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        border-radius: 20px;
+        transform: translateY(-10px);
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,36 @@
 <script>
     import { Mapdetailtitle } from '$lib';
+    import { Lemmacard } from '$lib';
 </script>
 
+<ul class="lemmacard-container">
+    <Lemmacard imgname="lemmapngtest1"/>
+    <Lemmacard imgname="lemmapngtest2"/>
+    <Lemmacard imgname="lemmapngtest3"/>
+    <Lemmacard imgname="lemmapngtest1"/>
+    <Lemmacard imgname="lemmapngtest2"/>
+    <Lemmacard imgname="lemmapngtest3"/>
+</ul>
 <Mapdetailtitle />
 
 <style>
-    
+    .lemmacard-container {
+        overflow-x: scroll;
+        scroll-snap-type: x mandatory;
+        list-style-type: none;
+        width: 100vw;
+        padding: 20px;
+        margin: 0;
+        display: flex;
+        gap: 4vw;
+    }
+    @media (min-width: 700px) {
+        .lemmacard-container {
+            display: grid;
+            grid-template-columns: repeat(3, 1fr);
+            gap: 4vw;
+            width: max-content;
+            justify-self: center;
+        }
+    }
 </style>

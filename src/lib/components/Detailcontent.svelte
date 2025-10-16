635ad58b909b7367748658e9c980eb95bcb46ecf
FatimaZelay <fatima.elay@gmail.com>
2025-10-16 19:22:35 +0200

    feat: html van detailpagina #49

--- src/lib/components/Detailcontent.svelte
+++ src/lib/components/Detailcontent.svelte
@@ -3,3 +3,13 @@
     export let text = "Informatieve tekst";
     export let image = "";
   </script>
+  
+  <section>
+    {#if image}
+      <div class="image-container">
+        <img src={image} alt={title} anchor-name="--titel" />
+      </div>
+    {/if}
+    <h1>{title}</h1>
+    <p>{text}</p>
+  </section>

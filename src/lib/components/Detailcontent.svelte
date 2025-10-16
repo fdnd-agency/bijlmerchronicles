0ab88ec6632f656aac2ea1de7cf8803db0273db4
FatimaZelay <fatima.elay@gmail.com>
2025-10-16 19:25:30 +0200

    feat: css van detailpagina #49

--- src/lib/components/Detailcontent.svelte
+++ src/lib/components/Detailcontent.svelte
@@ -13,3 +13,46 @@
     <h1>{title}</h1>
     <p>{text}</p>
   </section>
+  <style>
+    section {
+      max-width: 800px;
+      margin: 0 auto;
+      padding: 2rem;
+      position: relative;
+    }
+    
+    .image-container {
+      display: flex;
+      justify-content: center;
+      position: relative;
+    }
+    
+    .image-container img {
+      width: 80%;
+      height: auto;
+      border-radius: 15px;
+      anchor-name: --titel;
+    }
+    
+
+    h1 {
+      position-anchor: --titel;
+      position: absolute;
+      top: anchor(--titel bottom);
+      left: anchor(--titel center);
+      translate: -50% -60%; /* verplaatst de titel iets omhoog en naar links zodat hij exact gecentreerd onderaan de afbeelding staat */
+      font-size: 2.5rem;
+      text-transform: uppercase;
+      color: var(--color-neutral-700);
+      text-align: center; 
+      margin: 0;
+
+    }
+
+
+    p {
+      font-size: 1rem;
+      line-height: 1.6;
+      color: var(--color-neutral-700);
+      margin-top: 2rem;
+    }

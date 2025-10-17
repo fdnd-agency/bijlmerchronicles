71a994f3301c5195514eff6e916374b1eda69d5b
Matthijs ten Brink <matthijstenbrink@gmail.com>
2025-10-17 14:48:15 +0200

    Wie zijn wij content toegevoegd #68
    Dummy tekst alvast toegevoegd voor de "wie zijn wij". Gebruik gemaakt van custom properties en hij blijft in het midden. Ook op mobile

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,5 @@
 // place files you want to import through the `$lib` alias in this folder.
 
-export { default as Mapdetailtitle } from './components/Mapdetailtitle.svelte'
 export { default as Searchbar } from './components/Searchbar.svelte'
 export { default as Mapdetailtitle } from './components/Mapdetailtitle.svelte';
 export { default as Detailcontent } from './components/Detailcontent.svelte';
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,26 @@
     import { Searchbar } from '$lib';
 </script>
 
-<Mapdetailtitle />
-
 <Searchbar />
 
+<h2>WIE ZIJN WIJ</h2>
+<p>Wij zijn een consortium van partners geworteld in Amsterdam-Zuidoost die zich inzetten om Amsterdam-Zuidoost beter op de hoogte te brengen.</p>
+<p>We werken samen om het bestaande werk van gidsen, kenners, kunstenaars en ondernemers te versterken die de waarde van deze buurt al hebben laten zien.</p>
+
+<Mapdetailtitle />
+
 <style>
+    h2 {
+        font-size: var(--heading-2);
+        font-family: var(--main-font-black);
+        text-align: center;
+    }
+    p {
+        font-size: var(--paragraph-3);
+        line-height: 1.6;
+        margin-bottom: 1.5rem;
+        justify-self: center;
+        max-width: 70vw;
+    }
     
 </style>

99aa3ec2d81033620680a1f6bfe945386a712e77
FatimaZelay <fatima.elay@gmail.com>
2025-10-21 23:14:51 +0200

    feat: html nav-bar #50

---
+++ src/lib/components/Navdetailpagina.svelte
@@ -0,0 +1,15 @@
+<div class="sidebar">
+  <h3>Inhoud</h3>
+  <div class="divider"></div>
+
+  <details>
+    <summary>Ipsum</summary>
+    <ul>
+      <li><a href="/help">Lorem Ipsum</a></li>
+      <li><a href="/map">Lorem Ipsum</a></li>
+      <li><a href="/navigatie">Lorem Ipsum</a></li>
+      <li><a href="/overons">Lorem Ipsum</a></li>
+      <li><a href="/wiki">Lorem Ipsum</a></li>
+    </ul>
+  </details>
+</div>

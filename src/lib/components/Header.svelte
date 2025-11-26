ad75e75d7dca4a52e275ade50607460457fd9a22
FatimaZelay <fatima.elay@gmail.com>
2025-11-26 16:06:36 +0100

    fix: code conflict fixen #93

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,13 +1,12 @@
 <script>
-    import logo from "$lib/assets/logo.avif";
-    import { HamburgerMenu } from "$lib";
+    import logo from '$lib/assets/logo.avif';
+    import { HamburgerMenu } from '$lib';
 </script>
 
 <nav>
+
     <div class="logo-container">
-        <a href="/"
-            ><span class="sr-only">Home</span><img src={logo} alt="" /></a
-        >
+        <a href="/"><span class="sr-only">Home</span><img src="{ logo }" alt=""></a>
     </div>
 
     <div class="links-nav-container">
@@ -19,87 +18,25 @@
     </div>
 
     <aside class="loginNsignup-container">
-        <a href="/" class="login-button"
-            ><span class="login-button-border">Inloggen</span></a
-        >
-        <a href="/" class="signup-button"
-            ><span class="signup-button-border">Aanmelden</span></a
-        >
+        <a href="/" class="login-button"><span class="login-button-border">Inloggen</span></a>
+        <a href="/" class="signup-button"><span class="signup-button-border">Aanmelden</span></a>
     </aside>
 
+    
     <div class="hamburger-container">
         <HamburgerMenu />
     </div>
+
 </nav>
 
 <style>
 @layer general-styling{
 
-/* --------------------------------------- General styling ---------------------------------------  */
-        :root{
-            --button-width: 8rem;
-            --button-height: 2rem;
-            --border-radius: 0.4rem;
-        }
-
-        nav{
-            height: var(--header-height);
-            min-height: 7.25rem;
-            width: 100%;
-            z-index: 10;
-            position: fixed;
-            top: 0;
-            display: flex;
-            align-items: center;
-            flex-direction: row;
-            justify-content: flex-start;
-        }
-
-        .logo-container{
-            flex-grow: 3;
-
-            img{
-                width: 20vw;
-                min-width: 9.25rem;
-                max-width: 19rem;
-            }
-
-        }
-
-        .logo-container, .links-nav-container, .loginNsignup-container, .hamburger-container{
-            z-index: 5;
-            margin: 0 1rem 0; 
-        }
-
-        a{
-            color: var(--color-secondary);
-            text-decoration: none;
-            font-size: var(--paragraph-size);
-
-            &:hover{
-                color: var(--pop-out-color-500);
-            }
-
-/* --------------------------------------- Login / 'aanmelden' Button styling ---------------------------------------  */
-            span{
-                width: 100%;
-                height: 100%;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                z-index: 5;
-                border-radius: var(--border-radius);
-            }
-
-            .login-button-border{
-                border: solid 0.1rem var(--color-secondary-alt);
-            }
-
-            .signup-button-border{   
-                border: solid 0.1rem var(--color-primary-darker);
-            }
-
-        }
+    /* --------------------------------------- General styling ---------------------------------------  */
+    :root{
+        --signup-login-button-width: 8rem;
+        --signup-login-button-height: 2rem;
+        --border-radius: 0.4rem;
     }
 
     nav{
@@ -179,7 +116,8 @@
 
     /* --------------------------------------- MEDIA QUERIES ---------------------------------------  */
 
-        @container header (width > 35.7rem) {
+    /* @container header (width > 570px) */
+    @media (min-width: 570px) {
 
         .loginNsignup-container{
             display: flex;
@@ -202,7 +140,7 @@
             }
         }
 
-            @container header (width > 44rem) {
+    }
 
     /* @container header (width > 810px) */
     @media (min-width: 810px) {
@@ -239,7 +177,10 @@
                 max-width: 7rem;
             }
 
-                @container header (width > 51rem) {
+            a:nth-child(3){
+                display: block;
+            }
+        }
 
     }
 
@@ -248,44 +189,18 @@
 
         .links-nav-container {
 
-                    @container header (width > 64rem) {
+            a:nth-child(4), 
+            a:last-child{
+                min-width: 3.5rem;
+                max-width: 5rem;
+                display: block;
+            }
 
         }
 
     }
 
-                        .links-nav-container {
-                            gap: 5vw;
-                            flex-grow: 2;
-
-                            a{
-                                min-width: 5.5rem;
-                                max-width: 7rem;
-                            }
-
-                            a:nth-child(3){
-                                display: block;
-                            }
-
-                        }
-
-                    }
-
-                        @container header (width > 101rem) {
-                            .links-nav-container {
-
-                                a:nth-child(4), a:last-child{
-                                    min-width: 3.5rem;
-                                    max-width: 5rem;
-                                    display: block;
-                                }
-
-                            }
-                        }
-
-        
-
-/* --------------------------------------- BUTTON STYLING ---------------------------------------  */
+    /* --------------------------------------- BUTTON STYLING ---------------------------------------  */
 
     .login-button, .signup-button{
         width: var(--signup-login-button-width);

f194ae00c5cb6d5705fd61bd0ad55f604734aa9e
FatimaZelay <fatima.elay@gmail.com>
2026-01-19 13:28:42 +0100

    fix: juiste data laden vanuit API #11 & #126

--- src/routes/overons/+page.svelte
+++ src/routes/overons/+page.svelte
@@ -1,32 +1,20 @@
 <script>
+    import TeamCard from "$lib/components/molecules/TeamCard.svelte";
     import TeamSection from "$lib/components/organisms/TeamSection.svelte";
-  
+
+    export let data;
+    const { members = [] } = data;
+
     const subtitle =
-      "We werken samen om het bestaande werk van gidsen, kenners, kunstenaars en ondernemers te versterken die de waarde van deze buurt al hebben laten zien.";
-  
-    const teamMembers = [
-      {
-        name: "Graziella Hunsel Rivero",
-        role: "Project Manager",
-        bio: "Lorem Ipsum",
-        image: "/images/Graziella.png",
-        links: []
-      },
-      {
-        name: "Candice Karikari",
-        role: "Student",
-        bio: "Lorem Ipsum",
-        image: "/images/Candice.png",
-        links: []
-      },
-      {
-        name: "Núria Arbonés Aran",
-        role: "Project Leider",
-        bio: "Lorem Ipsum",
-        image: "/images/Nuria.png"
-      }
-    ];
-  </script>
-  
-  <TeamSection title="Over Ons" {subtitle} members={teamMembers} />
-  
+        "We werken samen om het bestaande werk van gidsen, kenners, kunstenaars en ondernemers te versterken die de waarde van deze buurt al hebben laten zien.";
+</script>
+
+<TeamSection title="Over Ons" {subtitle} {members} />
+
+<!--
+  <section class="team-grid">
+    {#each members as m (m.id)}
+      <TeamCard member={m} />
+    {/each}
+  </section>
+  -->

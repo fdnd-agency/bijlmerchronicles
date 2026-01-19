e4a1aabd8cb840de8e9cb5afd46db250bc458824
FatimaZelay <fatima.elay@gmail.com>
2026-01-19 13:24:40 +0100

    feat: Over Ons Data ophalen #11 & #126

---
+++ src/routes/overons/+page.server.js
@@ -0,0 +1,11 @@
+export async function load() {
+    const res = await fetch(
+      "https://fdnd-agency.directus.app/items/emibazo_persoon?fields=*"
+    );
+  
+    const json = await res.json();
+  
+    return {
+      members: json.data ?? []
+    };
+  }

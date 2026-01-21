c4475e7b5595340d26db43832777e7b3cddc2509
Luuk <147055722+Lutrian1@users.noreply.github.com>
2026-01-21 16:13:49 +0100

    Update index.js

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,7 +2,6 @@
 export { default as Header } from './components/organisms/Header.svelte'
 export { default as MapPreview } from './components/molecules/MapPreview.svelte';
 export { default as SearchBar } from './components/molecules/SearchBar.svelte'
-export { default as DetailContent } from './components/organisms/DetailContent.svelte';
 export { default as NavDetailPagina } from './components/atoms/NavDetailPagina.svelte';
 export { default as LemmaCard } from './components/molecules/LemmaCard.svelte'
 

4c9b76d6e93b008ae2dfa010a612c52b6b6c7472
Naul <alun.yik@hva.nl>
2026-02-06 10:14:34 +0100

    format

--- eslint.config.js
+++ eslint.config.js
@@ -2,27 +2,27 @@ import js from '@eslint/js';
 import svelte from 'eslint-plugin-svelte';
 
 export default [
-  js.configs.recommended,
+    js.configs.recommended,
 
-  ...svelte.configs['flat/recommended'],
+    ...svelte.configs['flat/recommended'],
 
-  {
-    files: ['**/*.js'],
-    rules: {
-      // Code conventies
-      'eqeqeq': ['error', 'always'],
-      'prefer-const': 'error',
-      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-      'no-console': 'warn'
-    }
-  },
+    {
+        files: ['**/*.js'],
+        rules: {
+            // Code conventies
+            eqeqeq: ['error', 'always'],
+            'prefer-const': 'error',
+            'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+            'no-console': 'warn',
+        },
+    },
 
-  {
-    files: ['**/*.svelte'],
-    rules: {
-      // Svelte-specifiek
-      'svelte/no-at-html-tags': 'error',
-      'svelte/no-unused-svelte-ignore': 'error'
-    }
-  }
+    {
+        files: ['**/*.svelte'],
+        rules: {
+            // Svelte-specifiek
+            'svelte/no-at-html-tags': 'error',
+            'svelte/no-unused-svelte-ignore': 'error',
+        },
+    },
 ];
